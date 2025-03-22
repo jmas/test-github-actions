0ce9d3261a7b09ad5629b29f40cfcc8928cfa8cc
Alex Maslakov <jmas.ukraine@gmail.com>
2025-03-22 20:49:57 +0200

    Update my_file.py

--- my_file.py
+++ my_file.py
@@ -3,18 +3,18 @@ con = sqlite3.connect("./data/sample.db")
 
 cur = con.cursor()
 
-cur.execute("PRAGMA table_info(movie)")
+cur.execute("SELECT * FRPM movie")
 
 rows = cur.fetchall()
 
 print (len(rows))
 
-# cur.execute("CREATE TABLE movie(title, year, score)")
+cur.execute("CREATE TABLE movie(title, year, score)")
 
-# cur.execute("""
-#     INSERT INTO movie VALUES
-#         ('Monty Python and the Holy Grail', 1975, 8.2),
-#         ('And Now for Something Completely Different', 1971, 7.5)
-# """)
+cur.execute("""
+    INSERT INTO movie VALUES
+        ('Monty Python and the Holy Grail', 1975, 8.2),
+        ('And Now for Something Completely Different', 1971, 7.5)
+""")
 
 print ('Hello, world!')

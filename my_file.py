e440ab02bf315da9c1e9e323e07ee71b750e0173
Alex Maslakov <jmas.ukraine@gmail.com>
2025-03-22 20:35:23 +0200

    Update my_file.py

--- my_file.py
+++ my_file.py
@@ -2,12 +2,19 @@ import sqlite3
 con = sqlite3.connect("./data/sample.db")
 
 cur = con.cursor()
-cur.execute("CREATE TABLE movie(title, year, score)")
 
-cur.execute("""
-    INSERT INTO movie VALUES
-        ('Monty Python and the Holy Grail', 1975, 8.2),
-        ('And Now for Something Completely Different', 1971, 7.5)
-""")
+cur.execute("PRAGMA table_info(your_table_name)")
+
+rows = cursor.fetchall()
+
+print(rows.__dict__)
+
+# cur.execute("CREATE TABLE movie(title, year, score)")
+
+# cur.execute("""
+#     INSERT INTO movie VALUES
+#         ('Monty Python and the Holy Grail', 1975, 8.2),
+#         ('And Now for Something Completely Different', 1971, 7.5)
+# """)
 
 print ('Hello, world!')

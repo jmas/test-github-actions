817b2076103f91b474e5e98c6dc7f0d820e36351
Alex Maslakov <jmas.ukraine@gmail.com>
2025-03-22 20:28:04 +0200

    Update my_file.py

--- my_file.py
+++ my_file.py
@@ -1,1 +1,13 @@
+import sqlite3
+con = sqlite3.connect("./data/sample.db")
+
+cur = con.cursor()
+cur.execute("CREATE TABLE movie(title, year, score)")
+
+cur.execute("""
+    INSERT INTO movie VALUES
+        ('Monty Python and the Holy Grail', 1975, 8.2),
+        ('And Now for Something Completely Different', 1971, 7.5)
+""")
+
 print ('Hello, world!')
